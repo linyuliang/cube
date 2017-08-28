6cbe7012425ba50ac6eb86bde44aad71e0f8c1da
linyuliang <linyuliang.85@gmail.com>
2017-08-28 17:42:17 +0800

    mybatis-plus generator demo submit

--- cube-generator/src/main/java/com/lyl/cube/generator/DaoGenerator.java
+++ cube-generator/src/main/java/com/lyl/cube/generator/DaoGenerator.java
@@ -1,22 +1,24 @@
 package com.lyl.cube.generator;
+
+import com.baomidou.mybatisplus.enums.FieldFill;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.FileOutConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.TemplateConfig;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
-import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.GlobalConfig;
-import com.baomidou.mybatisplus.generator.config.PackageConfig;
-import com.baomidou.mybatisplus.generator.config.StrategyConfig;
-import com.baomidou.mybatisplus.generator.config.rules.DbType;
-import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 public class DaoGenerator {
   /**
@@ -29,13 +31,13 @@ public class DaoGenerator {
 
     // 全局配置
     GlobalConfig gc = new GlobalConfig();
-    gc.setOutputDir("D://");
-    gc.setFileOverride(true);
-    gc.setActiveRecord(true);
+    gc.setOutputDir("/develop/code/");//输出目录
+    gc.setFileOverride(true);// 是否覆盖文件
+    gc.setActiveRecord(true);//开启 activeRecord 模式
     gc.setEnableCache(false);// XML 二级缓存
     gc.setBaseResultMap(true);// XML ResultMap
-    gc.setBaseColumnList(false);// XML columList
-    gc.setAuthor("Yanghu");
+    gc.setBaseColumnList(true);// XML columList
+    gc.setAuthor("linyuliang");
 
     // 自定义文件命名，注意 %s 会自动填充表实体属性！
     // gc.setMapperName("%sDao");
@@ -53,27 +55,35 @@ public class DaoGenerator {
       @Override
       public DbColumnType processTypeConvert(String fieldType) {
         System.out.println("转换类型：" + fieldType);
+        // if ( fieldType.toLowerCase().contains( "tinyint" ) ) {
+        //    return DbColumnType.BOOLEAN;
+        // }
         // 注意！！processTypeConvert 存在默认类型转换，如果不是你要的效果请自定义返回、非如下直接返回。
         return super.processTypeConvert(fieldType);
       }
     });
-    dsc.setDriverName("com.mysql.jdbc.Driver");
+    dsc.setDriverName("org.mariadb.jdbc.Driver");
     dsc.setUsername("root");
-    dsc.setPassword("521");
-    dsc.setUrl("jdbc:mysql://127.0.0.1:3306/mybatis-plus?characterEncoding=utf8");
+    dsc.setPassword("admin");
+    dsc.setUrl("jdbc:mysql://127.0.0.1:3306/cube?characterEncoding=utf8");
     mpg.setDataSource(dsc);
+
+    // 自定义需要填充的字段
+    List<TableFill> tableFillList = new ArrayList<>();
+    tableFillList.add(new TableFill("ASDD_SS", FieldFill.INSERT_UPDATE));
 
     // 策略配置
     StrategyConfig strategy = new StrategyConfig();
     // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
-    strategy.setTablePrefix(new String[] { "tlog_", "tsys_" });// 此处可以修改为您的表前缀
+    // strategy.setDbColumnUnderline(true)//全局下划线命名
+    strategy.setTablePrefix(new String[]{"t_", "th_"});// 此处可以修改为您的表前缀
     strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
     // strategy.setInclude(new String[] { "user" }); // 需要生成的表
     // strategy.setExclude(new String[]{"test"}); // 排除生成的表
     // 自定义实体父类
     // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
     // 自定义实体，公共字段
-    // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+    strategy.setSuperEntityColumns(new String[]{"test_id", "age"});
     // 自定义 mapper 父类
     // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
     // 自定义 service 父类
@@ -88,12 +98,19 @@ public class DaoGenerator {
     // 【实体】是否为构建者模型（默认 false）
     // public User setName(String name) {this.name = name; return this;}
     // strategy.setEntityBuilderModel(true);
+    // 【实体】是否为lombok模型（默认 false）<a href="https://projectlombok.org/">document</a>
+    strategy.setEntityLombokModel(true);
+    // Boolean类型字段是否移除is前缀处理
+    // strategy.setEntityBooleanColumnRemoveIsPrefix(true)
+    strategy.setRestControllerStyle(true);
+    // strategy.setControllerMappingHyphenStyle(true)
     mpg.setStrategy(strategy);
 
     // 包配置
     PackageConfig pc = new PackageConfig();
     pc.setParent("com.baomidou");
     pc.setModuleName("test");
+    pc.setController("controller");// 这里是控制器包名，默认 web
     mpg.setPackageInfo(pc);
 
     // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
@@ -108,15 +125,13 @@ public class DaoGenerator {
 
     // 自定义 xxList.jsp 生成
     List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
-    focList.add(new FileOutConfig("/template/list.jsp.vm") {
-      @Override
-      public String outputFile(TableInfo tableInfo) {
-        // 自定义输入文件名称
-        return "D://my_" + tableInfo.getEntityName() + ".jsp";
-      }
-    });
-    cfg.setFileOutConfigList(focList);
-    mpg.setCfg(cfg);
+//    focList.add(new FileOutConfig("/template/list.jsp.vm") {
+//      @Override
+//      public String outputFile(TableInfo tableInfo) {
+//        // 自定义输入文件名称
+//        return "D://my_" + tableInfo.getEntityName() + ".jsp";
+//      }
+//    });
 
     // 调整 xml 生成目录演示
     focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
